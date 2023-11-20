67a1f1b52084782e455f00a969f90753a1107f1b
arleth cordero <arleth.cordero09@gmail.com>
2023-11-19 23:55:56 -0300

    bd

--- backend/server-io/server/server.ts
+++ backend/server-io/server/server.ts
@@ -2,6 +2,7 @@ import Console from 'console';
 import express from 'express';
 import http from 'http';
 import { Server as SocketServer, Socket } from 'socket.io';
+import axios from 'axios'
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
@@ -10,17 +11,29 @@ const io: SocketServer = new SocketServer(server, {
         origin: 'http://localhost:5173',
     },
 });
-
 io.on('connection', (socket: Socket) => {
     Console.log(socket.id);
+
     socket.on('chat', (body: string) => {
         console.log(body);
+        // Envía el mensaje a Express para que lo guarde en la base de datos
+        axios.post('http://localhost:3000/save-message', { body })
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.error('Error al enviar mensaje a Express', error);
+            });
+
+        // Emite el mensaje a todos los clientes conectados
         socket.broadcast.emit('chat', {
-            body,
+            body: body,
             from: socket.id.slice(6),
         });
     });
 });
 
+
 server.listen(3100);
 console.log('List', 3100);
+
