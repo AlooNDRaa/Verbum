33f58a076aec7c92635ba2214c92346790437193
Alondra <alonngadea@gmail.com>
2023-12-04 00:15:18 -0300

    Refactor unit tastes with jest

---
+++ frontend/chat/src/assets/testing/sendMenssages.test.ts
@@ -0,0 +1,13 @@
+describe('Testing sum', () => {
+   function sum(a: number, b: number) {
+      return a + b;
+   }
+
+   it('should equal 4',()=>{
+      expect(sum(2,2)).toBe(4);
+     })
+
+   test('also should equal 6', () => {
+       expect(sum(2,4)).toBe(4);
+     }) 
+});
