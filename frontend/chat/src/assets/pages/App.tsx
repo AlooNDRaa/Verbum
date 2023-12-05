06eef175c5e60a5e50bab9ef743104febf2e51cd
Gadea Alondra <alonngadea@gmail.com>
2023-12-05 12:34:01 -0300

    Fixing react router dom and authenticated token to login

--- frontend/chat/src/assets/pages/App.tsx
+++ frontend/chat/src/assets/pages/App.tsx
@@ -1,52 +1,37 @@
-import { Route, BrowserRouter, Routes, Navigate } from 'react-router-dom';
-import { FC, useEffect, useState } from 'react';
-import { ThePrivatePage } from './private';
 import '../Styles/index.css';
+import { FC, useEffect, useState } from 'react';
+import { Route, BrowserRouter, Routes, Navigate } from 'react-router-dom';
 import { GameCYR } from './gamep';
-import { Error404 } from './errorpage';
 import { Login } from './login';
 import Chat from './chat';
 import Home from './home';
+import { Error404 } from './errorpage';
 import Blog from './blog';
-
+import { ThePrivatePage } from './private';
 interface AppProps {}
 
 const App: FC<AppProps> = (): JSX.Element => {
-  const [authenticated, setAuthenticated] = useState<boolean>(true);
+  const [authenticated, setAuthenticated] = useState<boolean>(false);
 
   useEffect(() => {
     const isAuthenticated = localStorage.getItem('login') !== null;
     setAuthenticated(isAuthenticated);
   }, []);
-
+  const PrivateRoute: FC<{ element: JSX.Element }> = ({ element }): JSX.Element => {
+    return authenticated ? element : <Navigate to="/"/>;
+  };
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/home" element={<PrivateRoute element={<Home />} />} />
+        <Route path="/chat" element={<PrivateRoute element={<Chat />} />} />
+        <Route path="/game" element={<PrivateRoute element={<GameCYR />} />} />
+        <Route path="/blog" element={<PrivateRoute element={<Blog/>}/>} />
+        <Route path="/priv" element={<PrivateRoute element={<ThePrivatePage/>}/>} />
         <Route path="/" element={<Login />} />
-        <Route
-          path="/home"
-          element={authenticated ? <Home /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/chat"
-          element={authenticated ? <Chat /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/game"
-          element={authenticated ? <GameCYR /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/blog"
-          element={authenticated ? <Blog /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/priv"
-          element={authenticated ? <ThePrivatePage /> : <Navigate to="/" />}
-        />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
   );
 };
-
 export default App;
