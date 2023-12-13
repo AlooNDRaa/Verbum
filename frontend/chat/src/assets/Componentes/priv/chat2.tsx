1b4228defc6599042503bcc1e61d836c815d1083
Gadea Alondra <alonngadea@gmail.com>
2023-12-13 02:36:15 -0300

    Fixed ultimate changes

--- frontend/chat/src/assets/Componentes/priv/chat2.tsx
+++ frontend/chat/src/assets/Componentes/priv/chat2.tsx
@@ -1,20 +1,25 @@
 import Mensajes from "./mensajespriv";
 
-
-
 function Chat2() {
-
-  const backgroundImageUrl2: string = 'url(https://i.gifer.com/origin/56/5620089ec3ce11732302d655bae5063d_w200.gif)';
+  const backgroundImageUrl2: string = 'https://i.gifer.com/origin/56/5620089ec3ce11732302d655bae5063d_w200.gif';
+  
+  
+  const backgroundStyle = {
+    backgroundImage: `linear-gradient(rgba(10, 10, 10, 20), rgba(0, 0, 0, 0.7)), url(${backgroundImageUrl2})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+  };
 
   return (
     <div>
-    <div
-    style={{ backgroundImage: backgroundImageUrl2 }}
-    className='flex h-screen items-stretch bg-black bg-cover'>
-     <Mensajes/>
+      <div
+        style={backgroundStyle}
+        className='flex h-screen items-stretch'>
+        <Mensajes />
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
 export default Chat2;
